68b00582a80f2e9532a93bac2bfd9bc40795a991
suhunlin <lin.suhun@msa.hinet.net>
2023-05-26 10:41:50 +0800

    Add code logic to check Camera Permission

--- app/src/main/java/com/suhun/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/suhun/qrcodescanner/MainActivity.java
@@ -1,8 +1,13 @@
 package com.suhun.qrcodescanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.PatternMatcher;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==
+                PackageManager.PERMISSION_GRANTED){
+            initQRCode();
+        }else{
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 123);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==123){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                initQRCode();
+            }else{
+                finish();
+            }
+        }
     }
 
     public void sendMessageFun(View view){
+
+    }
+
+    private void initQRCode(){
 
     }
 }
