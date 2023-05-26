00e3eeebd8b15cb699723843f8db81615e4106b5
suhunlin <lin.suhun@msa.hinet.net>
2023-05-26 10:46:27 +0800

    Import QRCode suite from https://github.com/dm77/barcodescanner

--- app/src/main/java/com/suhun/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/suhun/qrcodescanner/MainActivity.java
@@ -8,9 +8,16 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.PatternMatcher;
+import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private String tag = MainActivity.class.getSimpleName();
+    private ZXingScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +43,34 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
     public void sendMessageFun(View view){
 
     }
 
     private void initQRCode(){
+        mScannerView = new ZXingScannerView(this);
+    }
 
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v(tag, rawResult.getText()); // Prints scan results
+        Log.v(tag, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        // If you would like to resume scanning, call this method below:
+        mScannerView.resumeCameraPreview(this);
     }
 }
